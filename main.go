74111bb3a090aa0d73b0c163bbc388bf65874342
nickg <nickg@signalsciences.com>
2017-05-10 16:24:53 -0700

    Close #6 curl/wget abstraction, part of #7

--- main.go
+++ main.go
@@ -31,9 +31,36 @@ echo "specify version number or 'latest'"
 exit 1
 fi
 
+download() {
+  DEST=$1
+  SOURCE=$2
+  if which curl > /dev/null; then
+    WGET="curl -sSL"
+    if [ "${DEST}" != "-" ]; then
+      WGET="$WGET -o $DEST"
+    fi
+  elif which wget > /dev/null; then
+    WGET="wget -q -O $DEST"
+  else
+    echo "Unable to find wget or curl.  Exit"
+    exit 1
+  fi
+
+  # TODO: if source starts with github
+  #  and we have env auth token
+  #  then add it
+  HEADER=""
+  case $SOURCE in 
+  https://api.github.com*)
+     HEADER=""
+     ;;
+  esac
+  ${WGET} $HEADER $SOURCE
+}
+
 if [ "${VERSION}" = "latest" ]; then
   echo "Checking GitHub for latest version of ${OWNER}/${REPO}"
-  VERSION=$(curl -s https://api.github.com/repos/${OWNER}/${REPO}/releases/latest | grep -m 1 "\"name\":" | cut -d ":" -f 2 | tr -d ' ",')
+  VERSION=$(download - https://api.github.com/repos/${OWNER}/${REPO}/releases/latest | grep -m 1 "\"name\":" | cut -d ":" -f 2 | tr -d ' ",')
   if [ -z "${VERSION}" ]; then
     echo "Unable to determine latest release for ${OWNER}/${REPO}"
     exit 1
@@ -63,16 +90,7 @@ esac
 TARBALL=${NAME}.${FORMAT}
 URL=https://github.com/${OWNER}/${REPO}/releases/download/v${VERSION}/${TARBALL}
 
-if which curl > /dev/null; then
-  WGET="curl -sSL -o"
-elif which wget > /dev/null; then
-  WGET="wget -q -O"
-else
-  echo "Unable to find wget or curl.  Exit"
-  exit 1
-fi
-
-${WGET} ${TMPDIR}/${TARBALL} ${URL}
+download ${TMPDIR}/${TARBALL} ${URL}
 tar -C ${TMPDIR} -xzf ${TMPDIR}/${TARBALL}
 install -d ${BINDIR}
 install ${TMPDIR}/${BINARY} ${BINDIR}/
