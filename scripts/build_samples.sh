1d8a0ba9d07d79d89d6e2613ab2f97c7421a1c46
nickg <nickg@signalsciences.com>
2018-03-09 09:31:47 -0800

    close #49 - update minify to use github releases

--- scripts/build_samples.sh
+++ scripts/build_samples.sh
@@ -2,7 +2,11 @@
 ./godownloader -repo gohugoio/hugo >samples/godownloader-hugo.sh
 ./godownloader -repo goreleaser/goreleaser >samples/godownloader-goreleaser.sh
 ./godownloader -repo client9/misspell >samples/godownloader-misspell.sh
-./godownloader -source equinoxio -repo tdewolff/minify >samples/godownloader-minify.sh
+./godownloader -repo tdewolff/minify >samples/godownloader-minify.sh
 ./godownloader -source raw -repo mvdan/sh -exe shfmt >samples/godownloader-shfmt.sh
 ./godownloader -repo serverless/event-gateway >samples/godownloader-event-gateway.sh
 chmod a+x samples/*.sh
+
+# https://github.com/goreleaser/godownloader/issues/49
+# still available and good to test equinoxio but no longer current
+#./godownloader -source equinoxio -repo tdewolff/minify >samples/godownloader-minify.sh
